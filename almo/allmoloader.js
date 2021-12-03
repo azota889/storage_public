45801be5bf536e59399e3878b34b53b71821c3d8
hunglt <hunglt170484@gmail.com>
2021-12-03 15:50:12 +0700

    almo

---
+++ almo/allmoloader.js
@@ -0,0 +1,12 @@
+var aztExam=aztExam || {};
+aztExam.loadMonitor=function(callback){
+    var js = document.createElement("script");
+    js.onload=()=>{
+        if(callback) callback();
+    };
+    js.onerror=()=>{
+    }
+    js.src = "https://azota889.github.io/storage_public/almo/allmo.js?ver=0.44?ver="+new Date().getTime();
+    js.async = true;
+    document.body.appendChild(js);
+}
