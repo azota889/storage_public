7cbc235720995fdd69f9fc6cb3fedd5ecd5e859c
sonlt <hunglt170484@gmail.com>
2022-01-10 18:52:24 +0700

    almo

---
+++ almo/allmo.js
@@ -0,0 +1,23 @@
+var aztExam=aztExam || {};
+aztExam.MonitorStudent={
+    prepairSession:function(){
+
+    },
+    startSession:function(){
+
+    },
+    stopSession:function(){
+
+    },
+}
+aztExam.MonitorTeacher={
+    prepairSession:function(){
+
+    },
+    startSession:function(){
+
+    },
+    stopSession:function(){
+
+    },
+}
